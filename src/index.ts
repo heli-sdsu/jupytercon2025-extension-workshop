b65bc8a09cacb03de51a29daaef5a4c37ed76f9f
henry-li-sdsu <heli@sdsu.edu>
2025-11-03 11:46:10 -0800

    Register widget with launcher

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,10 @@ import {
   JupyterFrontEnd,
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
-
+import { ICommandPalette } from '@jupyterlab/apputils';
+import { ILauncher } from '@jupyterlab/launcher';
 import { requestAPI } from './request';
-
+import { ImageCaptionMainAreaWidget } from './widget';
 /**
  * Initialization data for the jupytercon2025-extension-workshop extension.
  */
@@ -12,7 +13,14 @@ const plugin: JupyterFrontEndPlugin<void> = {
   id: 'jupytercon2025-extension-workshop:plugin',
   description: 'A JupyterLab extension that displays a random image and caption.',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
+  requires: [ICommandPalette, ILauncher],  // dependencies of our extension
+  activate: (
+      app: JupyterFrontEnd,
+      // The activation method receives dependencies in the order they are specified in
+      // the "requires" parameter above:
+      palette: ICommandPalette,
+      launcher: ILauncher
+  ) => {
     console.log('JupyterLab extension jupytercon2025-extension-workshop is activated!');
 
     requestAPI<any>('hello')
@@ -24,6 +32,24 @@ const plugin: JupyterFrontEndPlugin<void> = {
           `The jupytercon2025_extension_workshop server extension appears to be missing.\n${reason}`
         );
       });
+
+    //Register a new command:
+    const command_id = 'image-caption:open';
+    app.commands.addCommand(command_id, {
+      execute: () => {
+        // When the command is executed, create a new instance of our widget
+        const widget = new ImageCaptionMainAreaWidget();
+
+        // Then add it to the main area:
+        app.shell.add(widget, 'main');
+        // what does this return do?
+        return widget
+      },
+      label: 'View a random image & caption'
+    });
+
+    palette.addItem({ command: command_id, category: 'Tutorial' });
+    launcher.add({ command: command_id });
   }
 };
 
